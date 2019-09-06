138ee6418254fb90a2c165ece0fc6abb9346f1ca
shanreed <srtech5set@gmail.com>
2019-09-06 08:53:20 -0700

    fetched data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { tsImportEqualsDeclaration } from '@babel/types';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+      .then(res => res.json())
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
+}
 }
 
 export default App;
